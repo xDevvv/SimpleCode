32acf46e4a18efb971db19ede6e687a265db4ff2
xDevvv <150666513+xDevvv@users.noreply.github.com>
2024-05-07 21:39:09 +0800

    Add files via upload

---
+++ try.js
@@ -0,0 +1,65 @@
+// Example Data from User
+
+const input1 = document.getElementById('network-id');
+const input2 = document.getElementById('class');
+const input3 = document.getElementById('default-subnet-mask');
+const input4 = document.getElementById('new-subnet-mask');
+const input5 = document.getElementById('ip-address-range');
+const input6 = document.getElementById('increment');
+const input7 = document.getElementById('broadcast-address');
+
+const table = document.querySelector('.main-table'); 
+
+const btnDisplay = document.querySelector('.btn');
+
+
+
+
+// Main Function Button
+btnDisplay.addEventListener('click', () => {
+
+    
+    const dataStorage = [];
+
+    let data = {
+        networkId: input1.value,
+        class: input2.value,
+        defaultSubnetMask: input3.value,
+        newSubnetMask: input4.value,
+        ipAddressRange: input5.value,
+        increment: input6.value,
+        broadcastAddress: input7.value
+    }
+
+    dataStorage.push(data);
+
+    dataStorage.forEach((value) => {
+        table.insertAdjacentHTML('beforeend',
+        `
+            <tr>
+                <td>${value.networkId}</td>
+                <td>${value.class}</td>
+                <td>${value.defaultSubnetMask}</td>
+                <td>${value.newSubnetMask}</td>
+                <td>${value.ipAddressRange}</td>
+                <td>${value.increment}</td>
+                <td>${value.broadcastAddress}</td>
+            </tr>
+        ` 
+    )
+    })
+
+    input1.value = '';
+    input2.value = '';
+    input3.value = '';
+    input4.value = '';
+    input5.value = '';
+    input6.value = '';
+    input7.value = '';
+
+    console.log(dataStorage)
+
+    
+})
+
+
